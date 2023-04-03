e6dad527b92c05caa6b15bebacd87fd812bc5f22
Zacharia <ndunguzachary24@gmail.com>
2023-04-03 11:00:17 +0300

    Customized error messages and validation  in user model

--- models/User.js
+++ models/User.js
@@ -1,25 +1,28 @@
 import mongoose from "mongoose";
+import isEmail from "validator"
 
 const UserSchema = new mongoose.Schema({
   fullnames: {
     type: String,
-    required: true,
+    required: [true, "please enter your fullname"],
     unique: true,
   },
   username: {
     type: String,
-    required: true,
+    required: [true, "Please enter your Username"],
     unique: true,
   },
   email: {
     type: String,
-    required: true,
+    required: [true, "Please enter your Email"],
     unique: true,
+    lowercase:true,
+    validate: [isEmail, "Please enter a valid email "]
   },
   password: {
     type: String,
-    required: true,
-    minLength: 6,
+    required: [true, "please enter your password"],
+    minLength:[6, "Minimum passwor length is 6 characters"],
   },
 });
 export default mongoose.model("User", UserSchema);
